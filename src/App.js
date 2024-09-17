c4ec90b6a35f1782d5edd2ac24afe36203d8d8dd
sankalp <sankalpyerigeri20@gmail.com>
2024-09-17 23:51:17 +0530

    Initial setup done

--- src/App.js
+++ src/App.js
@@ -1,2 +1,38 @@
 import React from "react"
+import ReactDOM from "react-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import Browse from "./components/Browse";
+import Login from "./components/Login";
+import Body from "./components/Body";
 
+const AppLayout = () =>
+{
+    return( <>
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+    </>)
+}
+
+const appRouter = createBrowserRouter([
+    {
+        path : "/",
+        element : <AppLayout/>,
+        children : [
+            {
+                path : "/",
+                element : <Browse/>
+            },
+            {
+                path : "/Login",
+                element : <Login/>
+            }
+
+        ]
+    }
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter}/>)
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+const Body = () =>
+    {
+        return (
+            <>
+            <div className="font-bold text-white">
+                <p className="text-green-700">Namaste Everyone</p>
+            </div>
+            </>
+        )
+    };
+    
+    export default Body;
