bc19f2af0bf8930e23e857591990f57789200128
sankalp <sankalpyerigeri20@gmail.com>
2024-09-23 11:35:50 +0530

    Added the header component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,14 @@
+import Header from "./Header";
+
 const Body = () =>
     {
         return (
-            <>
-            <div className="font-bold text-white">
-                <p className="text-green-700">Namaste Everyone</p>
+           <div>
+            <div className="flex flex-column">
+                <Header/>
             </div>
-            </>
+            <p className="font-bold text-teal-800 relative">Namste Everyone, This is Body</p>
+           </div>
         )
     };
     
