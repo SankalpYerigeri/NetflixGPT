82f1a0a90be47d2de8228e4be86da405490cb971
sankalp <sankalpyerigeri20@gmail.com>
2024-09-24 00:35:11 +0530

    resolved a bug : usernmae and photo not getting updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,7 @@ const Body = () =>
             <div className="flex flex-column">
                 <Header/>
             </div>
-            <p className="font-bold text-teal-800 relative">Namste Everyone, This is Body</p>
+            <p className="font-bold text-teal-800 mt-20">Namste Everyone, This is Body</p>
            </div>
         )
     };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,23 @@
 import { useState, useRef } from "react";
 import Header from "./Header";
 import { validateData } from "../utils/validate";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 
 
 const Login = () =>
 {
-
+    const navigate = useNavigate();
     const [isLogin, setIsLogin] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
 
     const email=useRef(null);
     const password=useRef(null);
+    const name=useRef(null);
+    const dispatch = useDispatch();
 
     const handleSubmit = () =>
     {
@@ -29,6 +34,16 @@ const Login = () =>
                 .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
+                    updateProfile(user, {
+                    displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/160774479?v=4"
+                    }).then(() => {
+                        const {uid, email, displayName, photoURL} = auth.currentUser;
+                        dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}));
+                        navigate("/browse");
+                    }).catch((error) => {
+                        setErrorMessage(error.message);
+                    });
+                      
                     console.log(user);
                     // ...
                 })
@@ -71,7 +86,7 @@ const Login = () =>
         </div>
         <form onSubmit={(e)=>e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
             <h1 className="font-bold text-3xl py-4">{isLogin ? "Sign In" : "Sign Up"}</h1>
-            {!isLogin && <input type="text" placeholder="Name" className="p-2 my-4 w-full bg-gray-700"/>}
+            {!isLogin && <input ref={name} type="text" placeholder="Name" className="p-2 my-4 w-full bg-gray-700"/>}
             <input ref={email} type="text" placeholder="Email" className="p-2 my-4 w-full bg-gray-700"/>            
             <input ref={password} type="text" placeholder="Password" className="p-2 my-4 w-full rounded-lg bg-gray-700"/>
             <p className="text-red-600  font-bold">{errorMessage}</p>
