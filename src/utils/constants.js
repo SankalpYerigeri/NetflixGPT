89529d2ad8366ff0ad15ac7126e28c3f9a591ac9
sankalp <sankalpyerigeri20@gmail.com>
2024-09-24 23:54:00 +0530

    Got access to TMDB API

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGE1NDBiMDYzZmI0M2E3NWZjOWMzZWIyMzRhOGExZiIsIm5iZiI6MTcyNzIwMDk3OC41NzU1NzIsInN1YiI6IjY2ZjJmY2Q1YTgyYjAwNTcwMzI3MDNkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A3lDR5QBcT-omTBEljin47226ewsNmXKYLaLGtwjBHM'
+  }
+};
