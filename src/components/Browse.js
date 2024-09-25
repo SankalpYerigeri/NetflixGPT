b4f6bdd0e3e1d7a90d6e28f5861c959fccc84c7d
sankalp <sankalpyerigeri20@gmail.com>
2024-09-25 23:39:06 +0530

    Converted the movie fetching code to the hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,28 +1,12 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/movieSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 
 const Browse = () =>
 
-    {
-
-    const dispatch = useDispatch();
-
-    const nowPlayingMovies = async () =>
-    {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-        const json = await data.json();
-        dispatch(addNowPlayingMovies(json.results));
-    }
-
-    useEffect(()=>
-    {
-        nowPlayingMovies();
-    }, []);
-
+{
+    useNowPlayingMovies();
 
     return <div>
         <Header/>
