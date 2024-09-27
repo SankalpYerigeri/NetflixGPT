87de2d1b863b408f9f2a6b63fea4c9a9e44791d7
sankalp <sankalpyerigeri20@gmail.com>
2024-09-28 01:26:02 +0530

    Fetched the data from API and rendered YT video on the page

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,8 +1,40 @@
-const VideoBackground = () =>
-{
-    return (
-        <div>VideoBackground</div>
-    )
-}
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+const VideoBackground = ({ movieId }) => {
+  const getMovieTrailer = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/957452/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filteredData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const trailer = filteredData.length ? filteredData[0] : json.results[0];
+    console.log(trailer);
+  };
+
+  useEffect(() => {
+    getMovieTrailer();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/4CLE3pWAAr8?si=ofrnnkNg3yoHjfId&amp;controls=0"
+        title="YouTube video player"
+        
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        //referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
 export default VideoBackground;
