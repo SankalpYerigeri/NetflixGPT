14583af8353ed88750607aeba37784f8f886cbbc
sankalp <sankalpyerigeri20@gmail.com>
2024-09-28 01:24:23 +0530

    fetched API and rendered title, overview button

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,13 @@
-const VideoTitle = () =>
+const VideoTitle = ({title, overview}) =>
 {
     return (
-        <div>
-            VideoTitle
+        <div className="pt-36 px-12">
+            <h1 className="text-6xl font-bold">{title}</h1>
+            <p className="text-lg w-1/4 py-6">{overview}</p>
+            <div>
+                <button className="bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg"> ▶️ Play Now</button>
+                <button className=" mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">More Info</button>
+            </div>
         </div>
     )
 }
