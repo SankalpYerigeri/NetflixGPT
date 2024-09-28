3354b81a63258b4bf784c0e481187156f9a8ae4e
sankalp <sankalpyerigeri20@gmail.com>
2024-09-29 00:31:19 +0530

    added a fetaure that a trailer can be stored in a redux store

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { useEffect} from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
 
 const VideoBackground = ({ movieId }) => {
+
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  console.log("fROM Redux ", trailerVideo?.key);
+  const dispatch = useDispatch();
+  
   const getMovieTrailer = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/957452/videos?language=en-US",
@@ -15,6 +22,9 @@ const VideoBackground = ({ movieId }) => {
     );
     const trailer = filteredData.length ? filteredData[0] : json.results[0];
     console.log(trailer);
+    console.log(trailer.key);
+    dispatch(addTrailerVideo(trailer));    
+    
   };
 
   useEffect(() => {
@@ -22,11 +32,11 @@ const VideoBackground = ({ movieId }) => {
   }, []);
 
   return (
-    <div>
+    trailerVideo && <div>
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/4CLE3pWAAr8?si=ofrnnkNg3yoHjfId&amp;controls=0"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
