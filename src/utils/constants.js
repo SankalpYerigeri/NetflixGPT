7e34e7a6e4b102fd9847f6b83de109e8ac45735a
sankalp <sankalpyerigeri20@gmail.com>
2024-09-30 00:42:44 +0530

    Uodated Image CDN URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,5 @@ export const API_OPTIONS = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGE1NDBiMDYzZmI0M2E3NWZjOWMzZWIyMzRhOGExZiIsIm5iZiI6MTcyNzIwMDk3OC41NzU1NzIsInN1YiI6IjY2ZjJmY2Q1YTgyYjAwNTcwMzI3MDNkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A3lDR5QBcT-omTBEljin47226ewsNmXKYLaLGtwjBHM'
   }
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/original";
