53741b9a4a0a7a78c6fe62f7ef03a0cd02c358f2
sankalp <sankalpyerigeri20@gmail.com>
2024-09-29 11:23:32 +0530

    Created a Hook to fetch the trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -2,41 +2,20 @@ import { useEffect} from "react";
 import { API_OPTIONS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+
 
 const VideoBackground = ({ movieId }) => {
 
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-  console.log("fROM Redux ", trailerVideo?.key);
-  const dispatch = useDispatch();
   
-  const getMovieTrailer = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/957452/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
 
-    const filteredData = json.results.filter(
-      (video) => video.type === "Trailer"
-    );
-    const trailer = filteredData.length ? filteredData[0] : json.results[0];
-    console.log(trailer);
-    console.log(trailer.key);
-    dispatch(addTrailerVideo(trailer));    
-    
-  };
-
-  useEffect(() => {
-    getMovieTrailer();
-  }, []);
-
+  useMovieTrailer(movieId);
   return (
-    trailerVideo && <div>
-      <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+    <div className="w-screen">
+      <iframe 
+        className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player"
         
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
