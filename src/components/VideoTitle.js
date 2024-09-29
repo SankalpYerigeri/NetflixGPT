b26f23da1eb4fba9fb4468450c55cc3fc590f436
sankalp <sankalpyerigeri20@gmail.com>
2024-09-29 17:55:40 +0530

    Change in CSS

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,7 +1,7 @@
 const VideoTitle = ({title, overview}) =>
 {
     return (
-        <div className="pt-36 px-12">
+        <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
             <h1 className="text-6xl font-bold">{title}</h1>
             <p className="text-lg w-1/4 py-6">{overview}</p>
             <div>
