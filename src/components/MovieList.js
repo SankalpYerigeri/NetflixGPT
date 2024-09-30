c95e2c5065ebe86804aa2e0bdd62409c2cbf3d4b
sankalp <sankalpyerigeri20@gmail.com>
2024-09-30 12:05:01 +0530

    Hidden the scroll bar and some minor CSS changes

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,7 +9,7 @@ const MovieList = ({title, movies}) =>
         movies &&
         <div className="px-6 ">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
+      <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} imagePath={movie.poster_path} />
