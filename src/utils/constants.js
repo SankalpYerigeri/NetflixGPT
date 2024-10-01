bef6193a5c476e800a4070db5d19e0c416affe98
sankalp <sankalpyerigeri20@gmail.com>
2024-10-01 11:13:05 +0530

    added a BG URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,5 @@ export const API_OPTIONS = {
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/original";
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/85ff76db-39e5-423a-afbc-97d3e74db71b/null/IN-en-20240909-TRIFECTA-perspective_b22117e0-4610-4d57-a695-20f77d241a4a_large.jpg";
